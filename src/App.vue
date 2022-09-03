ec30050378b9aa6f9773f1f41ab56345cbeb895a
Igor Josiel <igorjosielvitalino@gmail.com>
2022-09-03 17:22:08 -0300

    fix: componentizando componente da listagem dos registros.

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,11 @@
 <template>
+  <div>
     <div class="text-center" id="slogan">
       <h1>Projeto Vue</h1>
       <h6 class="text-secondary">Gerador de nomes utilizando Vue.js, GraphQL e Node.</h6>
     </div>
     <div id="main">
-      <div class="container">
-        <div class="row">
-          <div class="col-md">
-            <AppItemList
-              title="Prefixos"
-              v-bind:items="prefixes"
-              v-on:addItem="addPrefix"
-              v-on:deleteItem="deletePrefix"
-            />
-          <div class="col-md">
-            <AppItemList
-              title="Sufixos"
-              v-bind:items="sufixes"
-              v-on:addItem="addSufix"
-              v-on:deleteItem="deleteSufix"
-            />
-          </div>
-        </div>
-        <br />
-        <h5>
-          Domínios <span class="badge bg-info">{{ domains?.length }}</span>
-        </h5>
-        <div class="card">
-          <div class="card-body">
-            <ul class="list-group">
-              <li class="list-group-item" v-for="domain in domains" v-bind:key="domain.name">
-                <div class="row">
-                  <div class="col-md">
-                    {{ domain.name }}
-                  </div>
-                  <div class="col-md text-end">
-                    <a class="btn bg-info" v-bind:href="domain.checkout" target="_blank">
-                      <span class="fa fa-shopping-cart" />
-                    </a>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
+      <DomainList />
     </div>
   </div>
 </template>
@@ -52,32 +13,12 @@
 <script>
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
-import AppItemList from "./components/AppItemList.vue";
+import DomainList from "./components/DomainList.vue";
 
 export default {
 	name: "App",
 	components: {
-		AppItemList,
-	},
-	data: function () {
-		return {
-			prefixes: ["Air", "Jet", "Flight"],
-			sufixes: ["Hub", "Station", "Mart"],
-		};
-	},
-	methods: {
-		addPrefix(prefix) {
-			this.prefixes.push(prefix);
-		},
-		deletePrefix(prefix) {
-			this.prefixes.splice(this.prefixes.indexOf(prefix), 1);
-		},
-		addSufix(sufix) {
-			this.sufixes.push(sufix);
-		},
-		deleteSufix(sufix) {
-			this.sufixes.splice(this.sufixes.indexOf(sufix), 1);
-		},
+		DomainList,
 	},
 	computed: {
 		domains() {
